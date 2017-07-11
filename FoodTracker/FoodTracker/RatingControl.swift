ab1cb9d57f035210808eb2b940ecc6325f8951c4
Sunil Khambaita <sunilkhambaita@gmail.com>
2017-07-10 20:12:58 -0400

    Added accessibility information to the app

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -58,19 +58,13 @@ import UIKit
         }
         else {
             // Otherwise set the rating
+            rating = selectedRating
         }
         
     }
     
     // MARK: Private Methods
     private func setUpButtons() {
-        
-        // Loading button images
-        let bundle = Bundle(for: type(of: self))
-        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
-        let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
-        let highlightedStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
-        
         
         // Clearing any existing buttons first
         for button in ratingButtons {
@@ -79,8 +73,14 @@ import UIKit
         }
         ratingButtons.removeAll()
         
+        // Loading button images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        
         // Starting fresh buttons creation based on the starCount value
-        for _ in 0..<starCount {
+        for index in 0..<starCount {
             
             // Creating the button
             let button = UIButton()
@@ -93,6 +93,9 @@ import UIKit
             button.translatesAutoresizingMaskIntoConstraints = false
             button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
             button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            
+            // Setting the accessibility level
+            button.accessibilityLabel = "Set \(index + 1) star rating"
             
             // Setting up the button action
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
@@ -112,6 +115,33 @@ import UIKit
             
             // If the index of a button is less than the rating, that button should be selected.
             button.isSelected = index < rating
+            
+            // Setting a hint string for the currently selected star
+            let hintString: String?
+            
+            if rating == index + 1 {
+                hintString = "Tap to reset the rating to zero."
+            }
+            else {
+                hintString = nil
+            }
+            
+            // Calculate the value string
+            let valueString: String
+            
+            switch (rating) {
+            case 0:
+                valueString = "No rating set."
+            case 1:
+                valueString = "1 star set."
+            default:
+                valueString = "\(rating) stars set."
+            }
+            
+            // Assigning the hint string and value string
+            button.accessibilityHint = hintString
+            button.accessibilityValue = valueString
+            
         }
     }
 }
