fe4169734717dcfd8d43152ffae0f195215770b2
Sunil Khambaita <sunilkhambaita@gmail.com>
2017-07-13 21:07:38 -0400

    Added a table view to display meals as a list

---
+++ FoodTracker/FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Sunil M Khambaita on 2017-07-12.
+//  Copyright © 2017 Sunil. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
